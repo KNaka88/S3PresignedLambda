1ef284184c4ff37305dd689d050539dd157ad646
Koji Nakagawa <lightupthesky0627@gmail.com>
2020-11-02 22:59:35 -0800

    Fixed the unit tests for StartMultipartUploadAsync caused by refactoring

--- S3Lambdas.Tests/UploadServiceTests.cs
+++ S3Lambdas.Tests/UploadServiceTests.cs
@@ -1,5 +1,4 @@
-﻿using Amazon.Lambda.APIGatewayEvents;
-using Amazon.Lambda.TestUtilities;
+﻿using Amazon.Lambda.TestUtilities;
 using Amazon.S3;
 using Amazon.S3.Model;
 using FluentAssertions;
@@ -8,7 +7,6 @@ using NUnit.Framework;
 using S3Lambdas.Models;
 using S3Lambdas.Services;
 using System;
-using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace S3Lambdas.Tests
@@ -36,9 +34,9 @@ namespace S3Lambdas.Tests
         [Test]
         public async Task StartMultipartUploadAsync_should_throw_when_no_fileName()
         {
-            (StartMultipartUploadRequest request, APIGatewayProxyRequest apiGatewayProxyRequest) = PrepareRequest(fileName: null, folderName: "Folder1");
+            var request = PrepareRequest(fileName: null, folderName: "Folder1");
 
-            Func<Task> act = async () => { await _uploadService.StartMultipartUploadAsync(apiGatewayProxyRequest, _context); };
+            Func<Task> act = async () => { await _uploadService.StartMultipartUploadAsync(request, _context); };
 
             await act.Should().ThrowAsync<ArgumentNullException>();
         }
@@ -46,35 +44,34 @@ namespace S3Lambdas.Tests
         [Test]
         public async Task StartMultipartUploadAsync_should_throw_when_fileName_is_empty()
         {
-            (StartMultipartUploadRequest request, APIGatewayProxyRequest apiGatewayProxyRequest) = PrepareRequest(fileName: "", folderName: "Folder1");
+            var request = PrepareRequest(fileName: "", folderName: "Folder1");
 
-            Func<Task> act = async () => { await _uploadService.StartMultipartUploadAsync(apiGatewayProxyRequest, _context); };
+            Func<Task> act = async () => { await _uploadService.StartMultipartUploadAsync(request, _context); };
 
             await act.Should().ThrowAsync<ArgumentNullException>();
         }
 
-
         [Test]
         public async Task StartMultipartUploadAsync_should_return_UploadId()
         {
-            (StartMultipartUploadRequest request, APIGatewayProxyRequest apiGatewayProxyRequest) = PrepareRequest(fileName: "TestFile.pdf", folderName: null);
+            var request = PrepareRequest(fileName: "TestFile.pdf", folderName: null);
 
-            var response = await _uploadService.StartMultipartUploadAsync(apiGatewayProxyRequest, _context);
+            var response = await _uploadService.StartMultipartUploadAsync(request, _context);
 
             await _s3.Received().InitiateMultipartUploadAsync(Arg.Is<InitiateMultipartUploadRequest>(a =>
                 a.BucketName == bucketName &&
                 a.Key == request.FileName
             ));
-            response.StatusCode.Should().Be(200);
-            response.Body.Should().Be("42");
+            response.Should().BeOfType<InitiateMultipartUploadResponse>();
+            response.UploadId.Should().Be("42");
         }
 
         [Test]
         public async Task StartMultipartUploadAsync_should_construct_key_with_folderName()
         {
-            (StartMultipartUploadRequest request, APIGatewayProxyRequest apiGatewayProxyRequest) = PrepareRequest(fileName: "TestFile.pdf", folderName: "Folder1");
+            var request = PrepareRequest(fileName: "TestFile.pdf", folderName: "Folder1");
 
-            var response = await _uploadService.StartMultipartUploadAsync(apiGatewayProxyRequest, _context);
+            var response = await _uploadService.StartMultipartUploadAsync(request, _context);
 
             await _s3.Received().InitiateMultipartUploadAsync(Arg.Is<InitiateMultipartUploadRequest>(a =>
                 a.BucketName == bucketName &&
@@ -82,20 +79,13 @@ namespace S3Lambdas.Tests
             ));
         }
 
-        private (StartMultipartUploadRequest request, APIGatewayProxyRequest proxyRequest) PrepareRequest(string fileName, string folderName)
+        private StartMultipartUploadRequest PrepareRequest(string fileName, string folderName)
         {
-            var startMultipartUploadRequest = new StartMultipartUploadRequest
+            return new StartMultipartUploadRequest
             {
                 FileName = fileName,
                 FolderName = folderName
             };
-
-            var apiGatewayProxyRequest = new APIGatewayProxyRequest
-            {
-                Body = JsonSerializer.Serialize(startMultipartUploadRequest)
-            };
-
-            return (startMultipartUploadRequest, apiGatewayProxyRequest);
         }
     }
 }
